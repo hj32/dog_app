74b959c9a00bbb1ed653219d26938a99290d7e93
Howard <devx1968@outlook.com>
2021-08-23 02:05:49 +0100

    dog_app_utils script updated

--- dog_app_utils.py
+++ dog_app_utils.py
@@ -1,13 +1,21 @@
+from numpy import False_
+
+
 class TruthCounter:
-    """
-    Class to store Truth table of results from decisions made by each algorithm
-    """
     
-    def __init__(self):
-        self.__true_positive__=0
-        self.__true_negative__=0
-        self.__false_positive__=0
-        self.__false_negative__=0
+    
+    def __init__(self,tp=0,tn=0,fp=0,fn=0):
+        """
+            Args:
+            tp(int): True Positive
+            tn(int): True Negative
+            fp(int): False Positive
+            fn(int): False Negative
+        """
+        self.__true_positive__=tp
+        self.__true_negative__=tn
+        self.__false_positive__=fp
+        self.__false_negative__=fn
         
     
     def add_true_positive(self):
@@ -30,16 +38,47 @@ class TruthCounter:
         Increment false negative count
         """
         self.__false_negative__+=1
+    
         
     def print_out(self):
         """
         Print out counts
+        Returns :
+        zero : Zero better than None
         """
-        print('{:<4}{:<7}{:<9}{:<4}'.format('TP\t','TN\t','FP\t','FN\t'))
-        print('{:<10}{:<10}{:<10}{:<10}'.format(self.__true_positive__,
+        print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format('TP','TN','FP','FN','ChkSum','Acc'))
+        print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(self.__true_positive__,
         self.__true_negative__,
         self.__false_positive__,
-        self.__false_negative__))
+        self.__false_negative__,
+        self.get_check_sum(),
+        np.round(self.get_accuracy(self.__true_positive__,self.__true_negative__,
+        self.__false_positive__,self.__false_negative__),4)))
+        return 0
+
+
+    def get_check_sum(self):
+        """
+        Return sum of all 4 counts
+        """
+        return self.__true_positive__ + self.__true_negative__+ self.__false_positive__ + self.__false_negative__
+    
+    def get_accuracy(self,true_positive,true_negative,false_positive,false_negative):
+        """
+        Args:
+        true_positive (int):  Count True Positives
+        true_negative (int):  Count True Negatives
+        false_positive (int):  Count False Positives
+        false_negative (int):  Count False Negatives
+        Returns:
+        Accuracy (float)
+        """
+        if(true_positive + true_negative+ false_positive + false_negative>0):
+            accuracy=(true_positive + true_negative) / (true_positive + true_negative+ false_positive + false_negative)
+        else:
+            accuracy=-1
+        return accuracy
+
     
 
 class Scoreboard:
@@ -49,11 +88,11 @@ class Scoreboard:
     def __init__(self):
         self.__dd_counter__=TruthCounter()# dog detector counter
         self.__hd_counter__=TruthCounter()# human detector counter
-        self.__db_counter__=TruthCounter()
+        self.__db_counter__=TruthCounter()# dog breed counter
         
     def set_dog_detector_score(self,  isDogActual=False, isDogPred=False):
         """
-        
+        Set dog detector score
         """
         if( isDogActual and isDogPred):
             self.__dd_counter__.add_true_positive()
@@ -65,6 +104,9 @@ class Scoreboard:
             self.__dd_counter__.add_true_negative()
             
     def set_human_detector_score(self,  isHumanActual=False, isHumanPred=False,):
+         """
+         set human detector score
+         """
          if( isHumanActual and isHumanPred):
             self.__hd_counter__.add_true_positive()
          elif(isHumanActual and not isHumanPred):
@@ -75,10 +117,14 @@ class Scoreboard:
             self.__hd_counter__.add_true_negative()
 
     def set_dog_breed_detector_score(self, breedPred="", breedActual=""):
+        """
+        Set breed detector score
+        """
         if( breedPred == breedActual):
             self.__db_counter__.add_true_positive()
         elif(breedPred != breedActual):
-            self.__db_counter__.add_false_negative()
+            self.__db_counter__.add_false_positive()
+    
         
     def print_out(self):
         print("Dog Detector")
@@ -87,3 +133,10 @@ class Scoreboard:
         self.__hd_counter__.print_out()
         print("Dog Breed Detector")
         self.__db_counter__.print_out()
+
+
+
+import numpy as np
+
+
+
